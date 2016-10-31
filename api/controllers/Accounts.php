e1242b54584c33e86846c85fd3aa4b969698417a
Costi <mail.la.costi@gmail.com>
2016-10-31 18:53:25 +0000

    'create account' works

--- api/controllers/Accounts.php
+++ api/controllers/Accounts.php
@@ -1,8 +1,6 @@
 <?php
     
-
 class Accounts {
-
     public function create() {
         $errors = array();
         if(isset($_POST["email"])) {
@@ -11,31 +9,28 @@ class Accounts {
             } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
                 $errors["email"] = "Email is invalid";
             }
-            
             $patternPassword = '/^[a-z0-9$#_]{5,15}$/i';
             if (empty($_POST["password"])) {
                 $errors["password"] = "Password is required";     
             } elseif (!preg_match($patternPassword, $_POST["password"])) {
                 $errors["password"] = "Password is invalid";
             }
-            
             if (empty($_POST["repassword"])) {
                 $errors["repassword"] = "Please retype the password";     
             } elseif ($_POST["password"] !== $_POST["repassword"]) {
                 $errors["repassword"] = "Not equal with password";    
             }
-            
             if (empty($errors)) {
-                $salt = '1#$2';
+                $salt = '$1$';
                 $_POST["password"] = crypt($_POST["password"], $salt);
-                
-                require "Users.php";
-                $usersModel = new Users();
+                require "models/UsersModel.php";
+                $usersModel = new UsersModel();
                 $userId = $usersModel->createUser($_POST);
-                
-                if($userId > 0) {
-                    echo '<div>SUCCESS!!! login here <a href="login.php">Login</a></div>';
+                if($userId) {
+                    return json_encode($usersModel->getUserById($userId));
                 }
+            } else {
+                return $errors;
             }
         
         }
--- api/models/UsersModel.php
+++ api/models/UsersModel.php
@@ -1,11 +1,7 @@
 <?php
-<<<<<<< HEAD:api/models/UsersModel.php
 
 require_once "db.php";
-=======
-require "api/models/UsersModel.php";
-require_once "../models/db.php";
->>>>>>> 5f20ec863127646acf58aa6717e519e6e69e6eb7:api/controllers/Users.php
+require_once "controllers/Accounts.php";
 
 class UsersModel extends DB {
     public function createUser($user) {
@@ -15,12 +11,18 @@ class UsersModel extends DB {
                    ':role' => $user["role"]];
                    
         $sql = 'INSERT INTO users(name, email, password, role) VALUES(:name, :email, :password, :role)';
-        $sth = $this->dbh->prepare();
+        $sth = $this->dbh->prepare($sql);
         $sth->execute($params);
         
         return $this->dbh->lastInsertId();
     }
     
+    function getUserById($id) {
+        $sql = "SELECT * FROM users WHERE id=" . $id;
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute();
+        return $sth->fetch(PDO::FETCH_ASSOC);
+    }
     public function updateUser() {
         if (isset($_POST["id"])) {
             if (isset($_POST["email"]) || isset($_POST["password"])) {
