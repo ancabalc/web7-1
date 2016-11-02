43910911b9c788037fb731723d810af0ccf2af7d
Alexandru <alex.minecan@gmail.com>
2016-11-02 17:16:36 +0000

    Completed Login UI

---
+++ UI/models/Users.js
@@ -0,0 +1,28 @@
+function Users() {
+    
+}
+
+Users.prototype.loginUser = function(email,password) {
+        
+        var ajaxOptions = {
+            url:"https://web7-1-alecsandrul.c9users.io/api/accounts/login",
+            type:"POST",
+            dataType:"json",
+            data:{
+                email:email,
+                password:password
+            },
+            success:function(resp){
+                window.loginResp = resp;
+                window.currentUser = resp;
+                console.log("You're logged in!");
+            },
+            error:function(xhr,status,errorMessage){
+                console.log("Error status:"+status);
+            },
+            complete:function(){
+                console.log("AJAX Req has completed");
+            }
+        };
+        return $.ajax(ajaxOptions);
+}
