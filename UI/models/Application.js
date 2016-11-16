74910953dd9090b5e408441086c617d276c21232
andrei puscas <andrei.puscas@yahoo.com>
2016-11-16 17:59:09 +0000

    Application/applications list

---
+++ UI/models/Application.js
@@ -0,0 +1,25 @@
+/*global $*/
+function Application(options){
+    this.title = options.title;
+    this.description = options.description;
+    
+}
+
+Application.prototype.createApplication = function(){
+    var that = this;
+    // return $.ajax({
+    //         url:"/api/applications/create",
+    //         type:"POST",
+    //         dataType:"json",
+    //         success:function(resp){
+    //             for(var i = 0; i<resp.length; i++){
+    //                   var application = new Application(resp[i]);
+    //                   that.models.push(application);
+    //             }
+    //         },
+    //         error:function(xhr,status,errorMessage){
+    //             console.log("Error status:"+status);
+    //         }
+    // });
+    
+}
